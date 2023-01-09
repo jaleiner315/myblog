a7ecfc3ede5336438743a624176fdc01cbb2eb24
jaleiner315 <jra2201@columbia.edu>
2023-01-09 12:07:51 -0500

    Day 1(Jan 9th)

---
+++ components/Home.vue
@@ -0,0 +1,17 @@
+
+<template>
+	<div>
+    <center>
+		<h1>Hey y'all!</h1>
+		<p> Follow me on Instagram </p>
+		<a href="https://www.instagram.com/julie.aleiner/" target="_blank">
+			here's the link </a>
+        <p>Follow me on Linkedin </p>
+        <a href="https://www.linkedin.com/in/julie-aleiner-27931522a/" target="_blank">
+			here's the link </a>
+        <br>
+        <br>
+        <img src="Bridgepic.jpg" alt="Bridge pic image" width="329.4" height="556.2">
+    </center>
+	</div>
+</template>
